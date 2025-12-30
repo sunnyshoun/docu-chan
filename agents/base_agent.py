09c2547e38e8169a656f598bb84ec463fd2fe353
SunnyShoun <69196119+sunnyshoun@users.noreply.github.com>
2025-12-30 22:55:29 +0800

    delete unneed files & add logger to BaseAgent

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from logging import Logger, getLogger, DEBUG, INFO, WARNING, ERROR, CRITICAL
 from config.setting import PROMPT_ROOT, AgentConfig, AGENT_CONFIGS
 from agents.models import ChatRequest
 from agents.llm_client import chat, async_chat
@@ -6,12 +7,14 @@ from agents.llm_client import chat, async_chat
 class BaseAgent:
     agent_config: AgentConfig
     prompt_dir: Path
+    logger: Logger
     messages = []
     
     def __init__(self, name: str):
         self.name = name
         self.prompt_dir = Path(PROMPT_ROOT) / name
         self.agent_config = AGENT_CONFIGS[name]
+        self.logger = getLogger(name)
 
     def chat(self, messages: list[dict], tools: list[callable] = None, format: str = None) -> str:
         request = ChatRequest(
@@ -44,3 +47,15 @@ class BaseAgent:
             api_url=self.agent_config.api_url,
             api_key=self.agent_config.api_key
         )
+        
+    def log(self, level: int, message: str):
+        LOG_LEVELS = {
+            1: DEBUG,
+            2: INFO,
+            3: WARNING,
+            4: ERROR,
+            5: CRITICAL
+        }
+        
+        log_level = LOG_LEVELS.get(level, INFO)
+        self.logger.log(log_level, f"[Agent {self.name}] {message}")
